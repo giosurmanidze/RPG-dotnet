136d9810aa50e34fd5e0717b04391b753bac84e4
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-04-05 01:05:47 +0400

    fix/register character service

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using dotnet_rpg.Services.CharacterService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
--- obj/Debug/net8.0/dotnet-rpg.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-rpg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-rpg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3508a50de77b512d1a7abb7b412a045ada3da31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a2996751efcc3966aff6fe005b210f18789c1d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-rpg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-rpg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
